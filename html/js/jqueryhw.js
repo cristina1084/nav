f7a19bbdebd8fdf67829e70379bf57e604f224d7
cristina1084 <cristina.johns1084@gmail.com>
2019-07-30 10:05:32 +0530

    events

--- html/js/jqueryhw.js
+++ html/js/jqueryhw.js
@@ -1,14 +1,31 @@
 $(document).ready(function(){
     $("#nav1").click(function(){
-        $("#hide").hide();
-        $("#show").hide();
+        $("#h51,p").show();
+        $("#hide,#show,#h61,#h62").hide();
         $("#h51").click(function(){
             $("p").css("color","blue");
         });
     });
 
     $("#nav2").click(function(){
-        $("#h51").hide();
+        $("#h61,#h62").show();
+        $("#hide,#show,#h51,p").hide();
+        $("#h61").mouseup(function(){
+           alert("Mouse up");
+          });
+        $("#h62").mousedown(function(){
+            alert("Mouse up");
+            });
+        $("#h63").hover(function(){
+            alert("Hover");
+        }, function(){
+            alert("Bye");
+        });
+    });
+
+    $("#nav3").click(function(){
+        $("#h51,#h61,#h62").hide();
+        $("#hide,#show,p").show();
         $("#hide").click(function(){
             $("p").hide();
           });
@@ -16,5 +33,5 @@ $(document).ready(function(){
           $("#show").click(function(){
             $("p").show();
           });
-    })
+    });
 });
